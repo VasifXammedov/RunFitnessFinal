43eaec6c62a70d23071d1529c013bcecb58d01ac
VasifXammedov <vasifvkh@code.edu.az>
2021-02-27 02:05:47 +0400

    Areas start

---
+++ RunFitness/Extensions/Extension.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RunFitness.Extensions
+{
+    public static class Extention
+    {
+        public static bool IsImage(this IFormFile file)
+        {
+            return file.ContentType.Contains("image/");
+        }
+
+        public static bool MaxSize(this IFormFile file, int kb)
+        {
+            return file.Length / 1024 <= kb;
+        }
+
+        public static async Task<string> SaveImgAsync(this IFormFile file, string root, string folder)
+        {
+            string path = root;
+            string fileName = Guid.NewGuid().ToString() + file.FileName;
+            string resultPath = Path.Combine(root, folder, fileName);
+
+            using (FileStream fileStream = new FileStream(resultPath, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+
+            return fileName;
+        }
+    }
+
+    public enum Roles
+    {
+        Admin,
+        Member
+    }
+}
